9fa5f43979f800da334b5c11df1045a0a9ba2688
suriya <suriyaganesh097@gmail.com>
2024-03-02 15:35:50 -0500

    Update Privacy policy

--- landing/pages/privacy.js
+++ landing/pages/privacy.js
@@ -11,10 +11,165 @@ const Privacy = () => {
             </Head>
             <Navbar />
             <main className="container mx-auto px-4">
-                <h1 className="text-4xl font-bold my-6">Privacy</h1>
-                <p>We're gonna be super private</p>
+                <h3><b>Privacy Policy</b></h3>
+
+                <p>Effective: January 02,2023</p>
+
+                <p>TurnUp, (referred to in this policy as “TurnUP”
+                    or “we” or “our”) is committed to maintaining the trust of our customers.
+                    Please read this policy (the “Privacy Policy”) carefully to learn more about
+                    TurnUp’s privacy practices and policies with respect to Customer Data (defined
+                    below) that is collected through the data export interface (“The Admin Panel”),
+                    and used through the TurnUp Internet application (the “Web App”). This Privacy
+                    Policy applies to “Personal Data,” which is Customer Data that is associated
+                    with an identified or identifiable natural person and is protected as personal
+                    data or personal information under applicable data protection law.</p>
+
+                <p><b>1. TurnUp IS A SERVICE PROVIDER FOR ITS
+                    CUSTOMERS</b></p>
+
+
+                <p>TurnUp customers, and not TurnUp, determine how
+                    and why Personal Data submitted to the TurnUp Service is used, either by or at
+                    the direction of such customers. With respect to such Personal Data, (1) TurnUp
+                    is a “service provider” under the California Consumer Privacy Act (“CCPA”).
+                    This Privacy Policy describes how TurnUp processes Personal Data as a Data
+                    Processor for the purpose of providing the TurnUp Service to our customers
+                    pursuant to the applicable data processing terms with those customers.</p>
+
+                <p>As Data Controllers, TurnUp’s customers are
+                    responsible for disclosing the rights of individuals (“Data Subjects”) with
+                    respect to their Personal Data and other information regarding the collection
+                    and use of that Personal Data, in accordance with CCPA, and other laws
+                    requiring such disclosures.</p>
+
+                <p><b>2. SCOPE OF THIS PRIVACY POLICY</b></p>
+
+                <p>This Privacy Policy covers the processing of
+                    Personal Data that TurnUp performs as a Data Processor for and on behalf of its
+                    customers.</p>
+
+                <p><b>3. PERSONAL DATA PROCESSED UNDER THIS
+                    POLICY</b></p>
+
+                <p>TurnUp acts as a Data Processor with respect to
+                    any Personal Data comprised in the Customer Data. By “Customer Data” we mean
+                    the content or information which individuals authorized by a customer submit to
+                    the TurnUp Service.</p>
+
+                <p><b>4. USE OF YOUR PERSONAL DATA</b></p>
+
+                <p>TurnUp uses the Personal Data to provide the
+                    TurnUp Service to our customers pursuant to the applicable data processing
+                    terms with those customers. This will include integration of the TurnUp
+                    Service.</p>
+
+                <p><b>5. DISCLOSING YOUR PERSONAL DATA</b></p>
+
+                <p>TurnUp does not disclose your Personal Data to
+                    third parties except where permitted or requested by our applicable customer,
+                    or where required by law.</p>
+
+                <p><b>No Sale of Personal Data:</b>TurnUp does
+                    not sell, rent or lease your Personal Data.</p>
+
+                <p><b>Service Providers:</b>TurnUp may
+                    disclose your Personal Data to our contracted service providers so that they
+                    can provide TurnUp with services, such as IT, system administration and
+                    hosting, and customer support.</p>
+
+                <p>TurnUp will, to the extent required by law and
+                    the applicable customer contract, obtain assurances before disclosing Personal
+                    Data to a subcontractor or third-party agent that the recipient will: (a) use
+                    the Personal Data only to assist TurnUp in providing, maintaining or improving
+                    the TurnUp Service, (b) provide at least the same level of protection for
+                    Personal Data as is required of TurnUp, and (c) notify TurnUp if the recipient
+                    is no longer able to provide the required protections. Upon notice, TurnUp will
+                    act promptly to endeavor to stop and remediate any unauthorized processing of
+                    Personal Data by the recipient. TurnUp will remain liable for onward transfers
+                    to its subcontractors and third-party agents.</p>
+
+                <p>&nbsp;</p>
+
+                <p><b>6. SECURITY OF YOUR PERSONAL DATA</b></p>
+
+                <p>As specified in the data processing terms with
+the applicable Data Controller, TurnUp takes reasonable and appropriate
+measures to protect your Personal Data from loss, misuse, and inappropriate
+access.  We use standard, industry-wide best practices such as firewalls,
+encryption of Customer Data at rest and in transit, and (in certain areas)
+Secure Socket Layers (“SSL”) to protect your information.  However, as
+effective as encryption technology is, no security system is impenetrable.
+TurnUp cannot guarantee the security of our databases, nor can we guarantee
+that information that is supplied won't be intercepted while being transmitted
+to TurnUp over the Internet.</p>
+
+                <p><b>7. DATA RETENTION</b></p>
+
+                <p>Storage periods depend on the data processing
+terms with our customers and their choices, type of Personal Data, purposes of
+its collection and processing, and applicable law.</p>
+
+                <p><b>8. CHILDREN</b></p>
+
+                <p>TurnUp does not direct the TurnUp Service and
+associated websites to children. We do not knowingly collect Personal Data from
+children under the age of 16. If you are a parent or guardian and believe your
+child has provided us with Personal Data without your consent, please contact
+us at <a></a><a>privacy@turnup.com.</a>[1]<a></a>&nbsp;<a>[2]</a>&nbsp;
+                </p>
+
+                <p><b>9. ADDITIONAL DISCLOSURES FOR CALIFORNIA
+                    RESID<a></a><a>ENTS</a><a>[3]</a>&nbsp;<a>[4]</a>&nbsp;</b></p>
+
+                <p><b>This section provides additional details about
+the personal information TurnUp collects and receives about California
+consumers and the rights afforded to such consumers under the California
+Consumer Privacy Act or “CCPA.”</b></p>
+
+                <p>For more details about the personal information
+TurnUp has collected over the last 12 months, including the categories of
+sources, please see PERSONAL DATA PROCESSED UNDER THIS POLICY in Section 3
+above. TurnUp collects and receives this information for the business and
+commercial purposes described in USE OF YOUR PERSONAL DATA in Section 4 above.
+TurnUp discloses this information as described in DISCLOSING YOUR PERSONAL DATA
+in Section 5 above. TurnUp does not sell (as such term is defined in the CCPA)
+the personal information we collect (and will not sell it without providing a
+right to opt out).</p>
+
+                <p>Subject to certain limitations, the CCPA
+provides California consumers the right to request to know more details about
+the categories or specific pieces of personal information we collect (including
+how we use and disclose this information), to delete their personal
+information, to opt out of any “sales” that may be occurring, and to not be
+discriminated against for exercising these rights.</p>
+
+                <p>California consumers may make a request
+pursuant to their rights under the CCPA by sending us an email at <a
+                    href="mailto:privacy@turnup.com">privacy@turnup.com</a>.
+                    <a>[5]</a>&nbsp;<a>[6]</a>&nbsp;
+ We will verify your request and may request
+government identification or other information in order to do so. Consumers can
+also designate an authorized agent to exercise these rights on their behalf.</p>
+
+                <p><b>10. INTERNATIONAL DATA TRANSFERS</b></p>
+
+                <p>TurnUp is subject to the investigatory and
+enforcement powers of the U.S. Federal Trade Commission.</p>
+
+                <p><b>12. CHANGES/UPDATES</b></p>
+
+                <p>TurnUp reserves the right to update or modify
+this Privacy Policy at any time without prior notice. Any updates will be
+posted on the TurnUp website.</p>
+
+                <p>&nbsp;</p>
+
+                <p>&nbsp;</p>
+
+
             </main>
-            <Footer />
+            <Footer/>
         </>
     );
 }
